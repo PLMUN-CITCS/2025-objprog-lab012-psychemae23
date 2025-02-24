5d821a3039bf5a6df257d5440f5b7ffac369066f
Psyche Mae L. Colas <colaspsychemae_bscs@plmun.edu.ph>
2025-02-24 16:34:31 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,13 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+            //... (main and greet methods)..
+            greet(); // Calling the greet method
+            printMessage("This is my message!");
+        }
+            public static void greet() {
+                System.out.println("Hello from the greet method!"); 
+             }
+             public static void printMessage(String message) {
+                System.out.println(message);
+    }
+}
